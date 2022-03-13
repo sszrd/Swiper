bff29f6205ae741a0052d8e212247308ad7135fc
Wang Zijie <1327000313@qq.com>
2022-03-14 00:24:54 +0800

    finish button && debug for browser render queue error

--- Swiper.js
+++ Swiper.js
@@ -18,10 +18,20 @@ class Swiper {
         const swiper = document.createElement("div");
         swiper.className = "swiper";
         this.root.appendChild(swiper);
-
         this.width = swiper.getBoundingClientRect().width;
         this.height = swiper.getBoundingClientRect().height;
-
+        //创建左右切换按钮
+        const leftButton = document.createElement("div");
+        leftButton.className = "left-btn";
+        const rightButton = document.createElement("div");
+        rightButton.className = "right-btn";
+        leftButton.style.border = `solid ${this.width * 0.04}px`;
+        leftButton.style.borderColor = `transparent gray transparent transparent`;
+        leftButton.style.opacity = "0.5";
+        rightButton.style.border = `solid ${this.width * 0.04}px`;
+        rightButton.style.borderColor = `transparent transparent transparent gray`;
+        rightButton.style.opacity = "0.5";
+        //创建图片列表、下方导航列表
         this.oImgList = document.createElement("div");
         this.oImgList.className = "imgList";
         this.oNavList = document.createElement("div");
@@ -29,29 +39,36 @@ class Swiper {
         this.oImgList.style.width = `${this.imgList.length * 2}00%`;
         swiper.appendChild(this.oImgList);
         swiper.appendChild(this.oNavList);
-
+        swiper.appendChild(leftButton);
+        swiper.appendChild(rightButton);
+        //创建并初始化图片列表导航列表子元素
         for (let img of this.imgList) {
             const oImg = document.createElement("img");
             const oNav = document.createElement("span");
-
             oImg.src = img.src;
             oImg.alt = img.alt;
-
             oImg.style.width = `${this.width}px`;
             oImg.style.height = `${this.height}px`;
-
             oNav.style.width = `${this.width / this.imgList.length / 10}px`;
             oNav.style.height = `${this.height / this.imgList.length / 20}px`;
             oNav.style.margin = `${this.width / this.imgList.length / 15}px`;
-
             this.oImgList.appendChild(oImg);
             this.oNavList.appendChild(oNav);
         }
+        //复制一组图片实现无缝切换
         this.oImgList.innerHTML += this.oImgList.innerHTML;
-
+        this.oNavList.childNodes[0].className += " active";
+        //移动端触摸滑动事件绑定
         this.oImgList.addEventListener("touchstart", this.handleTouchStart);
         this.oImgList.addEventListener("touchmove", this.handleTouchMove);
         this.oImgList.addEventListener("touchend", this.handleTouchEnd);
+        //左右切换按钮事件绑定
+        leftButton.addEventListener("click", this.handleLeft);
+        leftButton.addEventListener("mouseover", () => leftButton.style.opacity = "1");
+        leftButton.addEventListener("mouseleave", () => leftButton.style.opacity = "0.5");
+        rightButton.addEventListener("click", this.handleRight);
+        rightButton.addEventListener("mouseover", () => rightButton.style.opacity = "1");
+        rightButton.addEventListener("mouseleave", () => rightButton.style.opacity = "0.5");
     }
 
     start() {
@@ -72,17 +89,10 @@ class Swiper {
     }
 
     next() {
+        this.ifArrivedLast();
         this.oImgList.style.transition = `${this.interval}s`;
         this.index++;
         this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
-        if (this.index === this.imgList.length * 2) {
-            this.end();
-            this.index = this.imgList.length - 1;
-            this.oImgList.style.transition = "none";
-            this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
-            this.time = this.delay;
-            this.start();
-        }
         this.setNavActive();
     }
 
@@ -98,14 +108,8 @@ class Swiper {
     handleTouchStart = (e) => {
         this.startX = e.changedTouches[0].pageX;
         this.end();
-        this.oImgList.style.transition = "none";
-        if (this.index === 0) {
-            this.index = this.imgList.length;
-            this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
-        } else if (this.index === this.imgList.length * 2 - 1) {
-            this.index = this.imgList.length - 1;
-            this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
-        }
+        this.ifArrivedFirst();
+        this.ifArrivedLast();
     }
 
     handleTouchMove = (e) => {
@@ -121,5 +125,40 @@ class Swiper {
         }
         this.oImgList.style.transition = `${this.interval / 1.5}s`;
         this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
+    }
+
+    handleLeft = () => {
+        this.end();
+        this.ifArrivedFirst();
+        this.index--;
+        this.oImgList.style.transition = `${this.interval}s`;
+        this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
+        this.setNavActive();
+        this.start();
+    }
+
+    handleRight = () => {
+        this.end();
+        this.next();
+        this.start();
+    }
+
+    ifArrivedFirst() {
+        if (this.index === 0) {
+            this.index = this.imgList.length;
+            this.oImgList.style.transition = "none";
+            this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
+            //强制刷新浏览器渲染队列
+            this.oImgList.offsetWidth;
+        }
+    }
+
+    ifArrivedLast() {
+        if (this.index === this.imgList.length * 2 - 1) {
+            this.index = this.imgList.length - 1;
+            this.oImgList.style.transition = "none";
+            this.oImgList.style.transform = `translate3D(${-this.index * this.width}px,0px,0px)`;
+            this.oImgList.offsetWidth;
+        }
     }
 }
